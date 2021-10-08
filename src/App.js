587fc5d5f32d5caafd8ca2c47eabde45128c2e46
Daehee Yoon <allendy11@gmail.com>
2021-10-08 21:43:23 +0900

    add client_test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import CocktailList from "./pages/CocktailList";
+import Community from "./pages/Community";
+import Mypage from "./pages/Mypage";
+import Mainpage from "./pages/Mainpage";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Mainpage />
+        </Route>
+        <Route path="/cocktaillist">
+          <CocktailList />
+        </Route>
+        <Route path="/community">
+          <Community />
+        </Route>
+        <Route path="/mypage">
+          <Mypage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
